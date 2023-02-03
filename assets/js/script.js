f6b262961c3896802b9672b9de5ecb96db727c13
mbatorek7 <maegan.batorek@valpo.edu>
2023-02-02 23:18:33 -0800

    some starter code for search bar

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+// assign API to a variable
+const APIKey = "84b79da5e5d7c92085660485702f4ce8";
+//history array
+var searchHistory = [];
+
+function findCity(city) {
+    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+
+}
